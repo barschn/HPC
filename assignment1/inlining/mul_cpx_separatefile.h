fd143f48b8e03bca6e23b1ec9284c2e6f9590496
tobias <tobmag@chalmers.se>
2018-09-11 13:11:07 +0200

    Working on sperate file

---
+++ assignment1/inlining/mul_cpx_separatefile.h
@@ -0,0 +1,6 @@
+#ifndef MUL_CPX_SEPARATEFILE_H_
+#define MUL_CPX_SEPARATEFILE_H_
+
+void mul_cpx_separatefile(double * a_re, double * a_im, double * b_re, double * b_im, double * c_re, double * c_im);
+
+#endif
