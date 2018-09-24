4d93daf8ae7f8e664f869187de7efb6418215054
tobias <tobmag@chalmers.se>
2018-09-24 14:21:53 +0200

    Meh

--- assignment1/valgrind/valg.c
+++ assignment1/valgrind/valg.c
@@ -9,9 +9,9 @@ void allocateints(){
 
 int main(){
 	allocateints();
-	int sum = 0;
+	long sum = 0;
 	for(size_t i=0; i < N; ++i)
 		sum+=i;
-	printf("sum = %d\n",sum);
+	printf("sum = %ld\n",sum);
 	return 0;
 }
