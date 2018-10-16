c3db7ba866f9ae4f1c753d6226340592feea8b01
barschn <43855508+barschn@users.noreply.github.com>
2018-10-16 13:29:34 +0200

    Add files via upload

--- Barbara/Assignment3/cell_distance.c
+++ Barbara/Assignment3/cell_distance.c
@@ -10,21 +10,27 @@
 
 // define points
 #define DIMENSION 3
-#define BLOCKSIZE 1000
+#define BLOCKSIZE 2000
+
+// values are 3d and between -10 and 10 (-1000 and 1000)
+// therefore the maximal distance between two points is 20 * sqrt(3) = 34.64
+// (2000 * sqrt(3) = 3464)
+// + 1 such that bouth extreme values are counted;
+#define MAXBINS 3465
 
 // define precision
 #define PRECISION 0.01
 
-// frequency matrix
-int **frequencies;
+// frequency vector
+int *frequencies;
 
 // thread number;
 int n_threads;
 
 // functions
-void read_block(FILE *fp, int **block, long int *size, long int startrowBlock1);
-void get_distance_within(int **block, long int *size);
-void get_distance_across(int **block1, long int *size1, int **block2, long int *size2);
+void read_block(FILE *fp, float **block, long int *size);
+void get_distance_within(float **block, long int *size);
+void get_distance_across(float **block1, long int *size1, float **block2, long int *size2);
 
 
 // main function
@@ -32,7 +38,6 @@ int main(int argc, char *argv[]) {
     
     // read input
     if (argc != 2) {
-        printf("Wrong input format.\n");
         return 0;
     }
     else {
@@ -43,27 +48,19 @@ int main(int argc, char *argv[]) {
     // intialize point matrix
     // note that we consider only integers
     // 01.56 -> 156 and so on ... for PRECISION 0.01
-    int *block1entries = (int*) malloc(sizeof(int) * BLOCKSIZE * DIMENSION);
-    int **block1 = (int**) malloc(sizeof(int*) * BLOCKSIZE);
-    int *block2entries = (int*) malloc(sizeof(int) * BLOCKSIZE * DIMENSION);
-    int **block2 = (int**) malloc(sizeof(int*) * BLOCKSIZE);
+    float *block1entries = (float*) malloc(sizeof(float) * BLOCKSIZE * DIMENSION);
+    float **block1 = (float**) malloc(sizeof(float*) * BLOCKSIZE);
+    float *block2entries = (float*) malloc(sizeof(float) * BLOCKSIZE * DIMENSION);
+    float **block2 = (float**) malloc(sizeof(float*) * BLOCKSIZE);
     for (size_t ix = 0, jx = 0; ix < BLOCKSIZE; ++ix, jx += DIMENSION) {
         block1[ix] = block1entries + jx;
         block2[ix] = block2entries + jx;
     }
     
     // initalize output matrix
-    // values are 3d between -10 and 10 (-1000 and 1000)
-    // therefore the maximal distance between two points is 20 * sqrt(3) = 34.64
-    // (2000 * sqrt(3) = 3464)
-    // + 1 such that bouth extreme values are counted
-    int max_bins = 3464 + 1;
-    int *frequencyentries = (int*) malloc(sizeof(int) * max_bins * 2);
-    frequencies = (int**) malloc(sizeof(int*) * max_bins);
-    for (size_t ix = 0, jx = 0; ix < max_bins; ++ix, jx += 2) {
-        frequencies[ix] = frequencyentries + jx;
-        frequencies[ix][0] = ix;
-        frequencies[ix][1] = 0;
+    frequencies = (int*) malloc(sizeof(int) * MAXBINS);
+    for (size_t ix = 0; ix < MAXBINS; ++ix) {
+        frequencies[ix] = 0;
     }
     
     
@@ -74,13 +71,14 @@ int main(int argc, char *argv[]) {
     
     long int *blocksize1 = malloc(sizeof(long int));
     long int *blocksize2 = malloc(sizeof(long int));
-    long int startlineBlock1 = 1, startlineBlock2 = 1;
+    long int startlineBlock1 = 1;
     
     // go through
     while (true) {
         
         // read the first row block of interest
-        read_block(fp, block1, blocksize1, startlineBlock1);
+        fseek(fp, 24 * (startlineBlock1 - 1), SEEK_SET);
+        read_block(fp, block1, blocksize1);
         
         // break if there is no data left
         if (*blocksize1 == 0) {
@@ -93,11 +91,8 @@ int main(int argc, char *argv[]) {
         // calculate the distances to all other column blocks successively
         while (true) {
             
-            // go to next block
-            startlineBlock2 += *blocksize1;
-            
             // read column block
-            read_block(fp, block2, blocksize2, startlineBlock2);
+            read_block(fp, block2, blocksize2);
             
             // go to next block row if no data left in that column
             if (*blocksize2 == 0) {
@@ -113,15 +108,14 @@ int main(int argc, char *argv[]) {
         // update start values
         // note that we only calculate the upper triangle of the distance matix
         startlineBlock1 += *blocksize1;
-        startlineBlock2 = startlineBlock1;
         
     }
     
     // print out the nonzero frequencies
-    for (size_t ix = 0; ix < max_bins; ++ ix) {
-        if (frequencies[ix][1] > 0) {
-            printf("%.2f\t%i\n", (double) frequencies[ix][0] * PRECISION, \
-                   frequencies[ix][1]);
+    for (size_t ix = 0; ix < MAXBINS; ++ ix) {
+        if (frequencies[ix] > 0) {
+            printf("%.2f %i\n", (double) ix * PRECISION, \
+                   frequencies[ix]);
         }
     }
     
@@ -133,9 +127,8 @@ int main(int argc, char *argv[]) {
     free(blocksize2);
     free(block2);
     free(block2entries);
-    free(frequencyentries);
     free(frequencies);
-    
+
     return 0;
     
 }
@@ -144,18 +137,17 @@ int main(int argc, char *argv[]) {
 
 // read block from datafile
 // one line has 24 characters
-void read_block(FILE *fp, int **block, long int *size, long int startrow) {
+void read_block(FILE *fp, float **block, long int *size) {
     
     int ix = 0;
     char line[24];
     char* end;
     
     // go to start row and read in values from block
-    fseek(fp, 24 * (startrow - 1), SEEK_SET);
     while (ix < BLOCKSIZE && fread(&line, sizeof(char), 24, fp)) {
-        block[ix][0] = round(strtod(line, &end) / PRECISION);
-        block[ix][1] = round(strtod(end, &end) / PRECISION);
-        block[ix][2] = round(strtod(end, NULL) / PRECISION);
+        block[ix][0] = strtof(line, &end);
+        block[ix][1] = strtof(end, &end);
+        block[ix][2] = strtof(end, NULL);
         ++ ix;
     }
     *size = ix;
@@ -167,19 +159,30 @@ void read_block(FILE *fp, int **block, long int *size, long int startrow) {
 
 // calculate all differences between points in the block
 // using get_eucledian_distance
-void get_distance_within(int **block, long int *size){
+void get_distance_within(float **block, long int *size){
     
-    #pragma omp parallel for collapse(2)
+    float r, d;
+    int f;
+    
+    #pragma omp parallel for collapse(2) reduction(+:frequencies[:MAXBINS]) private(r,d,f)
     for (size_t ix = 0; ix < *size; ++ix) {
         for (size_t jx = 0; jx < *size; ++jx) {
             
             if (ix < jx) {
-                int d = round(sqrt((block[ix][0] - block[jx][0]) * (block[ix][0] - block[jx][0]) + \
-                               (block[ix][1] - block[jx][1]) * (block[ix][1] - block[jx][1]) + \
-                               (block[ix][2] - block[jx][2]) * (block[ix][2] - block[jx][2])));
-            
-                #pragma omp atomic
-                ++ frequencies[d][1];
+                
+                r = (block[ix][0] - block[jx][0]) * (block[ix][0] - block[jx][0]) + \
+                    (block[ix][1] - block[jx][1]) * (block[ix][1] - block[jx][1]) + \
+                    (block[ix][2] - block[jx][2]) * (block[ix][2] - block[jx][2]);
+                
+                d = sqrt(r);
+                
+                f = (int) (d / PRECISION);
+                if (d - f < 0.5) {
+                    frequencies[f] += 1;
+                }
+                else {
+                    frequencies[f + 1] += 1;
+                }
             }
             
         }
@@ -191,19 +194,28 @@ void get_distance_within(int **block, long int *size){
 
 // calculate all differences between points in one block
 // to points in the other block
-void get_distance_across(int **block1, long int *size1, int **block2, long int *size2) {
+void get_distance_across(float **block1, long int *size1, float **block2, long int *size2) {
+
+    float r, d;
+    int f;
     
-    #pragma omp parallel for collapse(2)
+    #pragma omp parallel for collapse(2) reduction(+:frequencies[:MAXBINS]) private(r,d,f)
     for (size_t ix = 0; ix < *size1; ++ix) {
         for (size_t jx = 0; jx < *size2; ++jx) {
             
-            int d = round(sqrt((block1[ix][0] - block2[jx][0]) * (block1[ix][0] - block2[jx][0]) + \
-                           (block1[ix][1] - block2[jx][1]) * (block1[ix][1] - block2[jx][1]) + \
-                           (block1[ix][2] - block2[jx][2]) * (block1[ix][2] - block2[jx][2])));
-        
-            #pragma omp atomic
-            ++ frequencies[d][1];
+            r = (block1[ix][0] - block2[jx][0]) * (block1[ix][0] - block2[jx][0]) + \
+                (block1[ix][1] - block2[jx][1]) * (block1[ix][1] - block2[jx][1]) + \
+                (block1[ix][2] - block2[jx][2]) * (block1[ix][2] - block2[jx][2]);
             
+            d = sqrt(r);
+        
+            f = (int) (d / PRECISION);
+            if (d - f < 0.5) {
+                frequencies[f] += 1;
+            }
+            else {
+                frequencies[f + 1] += 1;
+            }
         }
     }
     
